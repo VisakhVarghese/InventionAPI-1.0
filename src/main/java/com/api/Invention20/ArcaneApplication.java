30824c0763ef718989c2afef8f0571c35e1ff361
Visakh Varghese <visakh.varghese@ibm.com>
2024-12-11 21:06:32 +0530

    Start of A Reinvention

---
+++ src/main/java/com/api/Invention20/ArcaneApplication.java
@@ -0,0 +1,13 @@
+package com.api.Invention20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Invention20/ArcaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Invention20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
